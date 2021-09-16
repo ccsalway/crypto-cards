1f6214c8cadca05c57cf0082a2e3939e2f01c6dc
Christian Salway <ccsalway@icloud.com>
2021-09-16 09:07:06 +0100

    reused request

--- modules/eth.js
+++ modules/eth.js
@@ -1,15 +1,13 @@
 import * as helpers from './helpers.js'
 
 
-export async function connect() {
-    return await window.ethereum.request({
-        method: 'eth_requestAccounts'
-    });
-}
-
 export async function request(method, params) {
     return await window.ethereum.request({
         method: method,
         params: params
     });
 };
+
+export async function connect() {
+    return request('eth_requestAccounts');
+}
